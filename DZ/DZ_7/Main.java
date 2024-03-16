848b1e68b2473ca9937ee58982d07df3382424d4
ScOrP <alex_77_90@mail.ru>
2024-03-16 20:34:29 +0500

    Дз 7 2.2.1

--- DZ/DZ_7/Main.java
+++ DZ/DZ_7/Main.java
@@ -1,14 +1,20 @@
 package DZ.DZ_7;
 
-import DZ.DZ_7.controller.NewController;
+import DZ.DZ_7.controller.Controller;
+import DZ.DZ_7.controller.ControllerDecorator;
 import DZ.DZ_7.model.Calk;
 import DZ.DZ_7.view.View;
 
 public class Main {
     public static void main(String[] args) {
-        NewController nc = new NewController(new Calk(), new View());
+        Controller controller = new Controller(new Calk(), new View());
+        ControllerDecorator controllerDecor = new ControllerDecorator(new Controller(new Calk(), new View()));
 
-        nc.buttonClick();
+        // controller.buttonClick();
+        controllerDecor.buttonClick();
+        controllerDecor.buttonClick();
+        controllerDecor.buttonClick();
+        controllerDecor.buttonClick();
 
     }
 }
